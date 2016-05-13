4aacdcbcd2528e2b4966d9eaf7f921435190a015
DericM <dmccadden@my.bcit.ca>
2016-05-13 16:18:54 -0700

    commented Main and Canvas

--- js/Components/Canvas.js
+++ js/Components/Canvas.js
@@ -1,72 +1,96 @@
 
-/* 
-Creates a canvas and inserts it into the container div in index.html.
+/**
+ Creates a canvas and inserts it into the container div in index.html.
 
-Author: Brody
-Date: Sat, 7
-*/
+ Author: Brody
+ Date: Sat, 7
+ */
 function Canvas(width, height) {
-	var width = width;
-	var height = height;
-	var drawables = [];
-	var canvas = document.createElement("canvas");
-	canvas.style.border = "1px solid black";
-	canvas.style.position = "absolute";
-	document.getElementById("container").appendChild(canvas);
-	canvas.setAttribute('width', width);
-	canvas.setAttribute('height', height);
-	var context = canvas.getContext("2d");
+    this.width = width;
+    this.height = height;
+    this.drawables = [];
 
-	this.getContext = function() {
-		return context;
-	};
-	
-	this.getCanvas = function () {
-		return canvas;
-	};
+    this.canvas = document.createElement("canvas");
+    this.canvas.style.border = "1px solid black";
+    this.canvas.style.position = "absolute";
+    this.canvas.setAttribute('width', this.width);
+    this.canvas.setAttribute('height', this.height);
 
-	this.getWidth = function() {
-		return width;
-	}
+    this.context = this.canvas.getContext("2d");
 
-	this.getHeight = function() {
-		return height;
-	}
-
-	/*
-		Adds a drawable to the end of the drawables array.
-
-		PARAM drawable: The drawable to be added.
-	*/
-	this.insertDrawable = function(drawable) {
-		drawables.push(drawable);
-		
-	};
-
-	/*
-		Calls the draw method on all the drawables in the
-		drawables array, which draws them all to the context
-		of this canvas.
-	*/
-	this.draw = function() {
-		context.clearRect(0, 0, canvas.width, canvas.height);
-		for (var i = 0; i < drawables.length; i++) {
-			drawables[i].draw();
-		}
-	};
-
-	/**
-	 * Sets the visibility of the canvas html element this canvas object is
-	 * associated with. Passing false removes it from the HTML dom tree,
-	 * passing true adds it to the HTML dom tree.
-	 *
-	 * @param {boolean} visibility: True to add to dom tree, false to remove from.
-     */
-	this.setVisible = function(visibility) {
-		if(visibility == false) {
-			document.getElementById("container").removeChild(canvas);
-		} else {
-			document.getElementById("container").appendChild(canvas);
-		}
-	}
+    document.getElementById("container").appendChild(this.canvas);
 }
+
+
+Canvas.prototype = {
+
+    /**
+     * Return context.
+     * @returns {CanvasRenderingContext2D}
+     */
+    getContext: function() {
+        return this.context;
+    },
+
+    /**
+     * Return canvas.
+     * @returns {Element}
+     */
+    getCanvas: function () {
+        return this.canvas;
+    },
+
+    /**
+     * Return width.
+     * @returns {*}
+     */
+    getWidth: function() {
+        return this.width;
+    },
+
+    /**
+     * Return height.
+     * @returns {*}
+     */
+    getHeight: function() {
+        return this.height;
+    },
+
+    /**
+     Adds a drawable to the end of the drawables array.
+
+     PARAM drawable: The drawable to be added.
+     */
+    insertDrawable: function(drawable) {
+        this.drawables.push(drawable);
+
+    },
+
+    /**
+     Calls the draw method on all the drawables in the
+     drawables array, which draws them all to the context
+     of this canvas.
+     */
+    draw: function() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        for (var i = 0; i < this.drawables.length; i++) {
+            this.drawables[i].draw();
+        }
+    },
+
+    /**
+     * Sets the visibility of the canvas html element this canvas object is
+     * associated with. Passing false removes it from the HTML dom tree,
+     * passing true adds it to the HTML dom tree.
+     *
+     * @param {boolean} visibility: True to add to dom tree, false to remove from.
+     */
+    setVisible: function(visibility) {
+        if(visibility == false) {
+            document.getElementById("container").removeChild(this.canvas);
+        } else {
+            document.getElementById("container").appendChild(this.canvas);
+        }
+    }
+
+};
